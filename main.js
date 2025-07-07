e20ad768f971d91ea3a9702f7b8dbece5d6b4052
zeratheuz <zeratheuz@gmail.com>
2025-07-07 16:51:14 -0300

    Update JS

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const Gameboard = (function () {
+  const rows = 4
+  const columns = 4
   const gameboard = []
 
+  for (let i = 0; i < rows; i++) {
+    gameboard[i] = []
+    for (let j = 0; j < columns; j++) {
+      gameboard[i].push(Cell())
+    } 
+  }
 })()
