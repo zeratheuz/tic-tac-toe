e1bf506232c25daa21c327261cdcb9cf6f8979cd
zeratheuz <zeratheuz@gmail.com>
2025-07-18 14:55:20 -0300

    Update JS

--- main.js
+++ main.js
@@ -78,6 +78,16 @@ function GameController(
     const checkWin = () => {
       const gameboardCopy = gameboard.getGameboard()
       const gameboardTokens = gameboardCopy.map((cellRow) => cellRow.map((cell => cell.getToken())))
+      const winPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ]
 
       console.log(gameboardTokens)
     }
